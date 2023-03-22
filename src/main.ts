90359357c28d36d102e93e3d0072becc26f6d4c2
Agnès Noléo <noleoa_github@gmail.com>
2023-03-22 18:18:41 +0100

    Adding a room + more détails 2203

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+// Calling bootstrapExtra will initiliaze all the "custom properties"  
+bootstrapExtra();
 
 console.log('Script started successfully');
 
@@ -17,14 +19,38 @@ WA.onInit().then(() => {
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
     })
     WA.room.area.onLeave('clock').subscribe(closePopup)
-
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
-}).catch(e => console.error(e));
 
+WA.room.area.onEnter('NosAgences').subscribe(() => {
+              currentPopup = WA.ui.openPopup("NosAgencesPopUp", "Salut toi", [
+
+
+{
+        label: "Fermer",
+        className: "normal",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }
+
+
+]);
+    })
+
+    WA.room.area.onLeave('NosAgences').subscribe(closePopup)
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+
+
+    }).catch(e => console.error(e));
 function closePopup(){
     if (currentPopup !== undefined) {
         currentPopup.close();
