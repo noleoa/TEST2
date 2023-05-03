4d143dda093f36a9f1a8d0ed7b1606a0b186fb01
Agnès Noléo <noleoa_github@gmail.com>
2023-05-03 12:17:03 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
+let currentPopup: Popup | undefined = undefined;
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
