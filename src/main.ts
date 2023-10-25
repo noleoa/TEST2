acefce716346109405a89950c47dcc6db80005b2
Agnès Noléo <noleoa_github@gmail.com>
2023-10-25 11:59:16 +0200

    TESTPPT1159

--- src/main.ts
+++ src/main.ts
@@ -133,7 +133,7 @@ WA.ui.actionBar.addButton({
   callback: () => {
     WA.ui.modal.openModal({
       title: "Tutoriel",
-      src: "https://noleoa.github.io/TEST2/Affiches/TESTPPT.pptx",
+      src: "https://noleoa.github.io/TEST2/Affiches/TESTPPT.pdf",
       allow: "fullscreen",
       allowApi: true,
       position: "right",
@@ -144,7 +144,7 @@ WA.ui.actionBar.addButton({
 
 WA.ui.modal.openModal({
   title: "Tutoriel",
-  src: "https://noleoa.github.io/TEST2/Affiches/TESTPPT.pptx",
+  src: "https://noleoa.github.io/TEST2/Affiches/TESTPPT.pdf",
   allow: "fullscreen",
   allowApi: true,
   position: "center",
