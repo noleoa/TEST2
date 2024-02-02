a6e93447640a8068fd8f5876551b50ef8320a231
Agnès Noléo <noleoa_github@gmail.com>
2024-02-02 15:54:17 +0100

    Chgt Map

--- src/main.ts
+++ src/main.ts
@@ -175,7 +175,7 @@ WA.ui.actionBar.addButton({
   callback: () => {
     WA.ui.modal.openModal({
       title: "Plan de la carte",
-      src: "https://noleoa.github.io/TEST2/Affiches/map3.png",
+      src: "https://noleoa.github.io/TEST2/Affiches/map3.pdf",
       allow: "fullscreen",
       allowApi: true,
       position: "center",
